fac4fde247222b0e3e769791810720643bd63e31
dratzymarcano <dratzymarcano@gmail.com>
2026-01-13 02:11:55 +0100

    Fix: Add sitemap stylesheet and link it in XML generation

--- astro-peptide/src/utils/sitemap.ts
+++ astro-peptide/src/utils/sitemap.ts
@@ -34,6 +34,7 @@ ${alternates}
  */
 export function generateSitemapXml(urls: SitemapURL[]): string {
   return `<?xml version="1.0" encoding="UTF-8"?>
+<?xml-stylesheet type="text/xsl" href="/sitemap-style.xsl"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
         xmlns:xhtml="http://www.w3.org/1999/xhtml">
 ${urls.map(generateUrlEntry).join('\n')}
